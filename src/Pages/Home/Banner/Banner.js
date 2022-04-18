e89cbcf49b55797fc23938fdda918b17c9b62084
Najmul Haque <najmul0612@gmail>
2022-04-18 22:23:22 +0600

    readme added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -19,8 +19,8 @@ const Banner = () => {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>My Chamber</h3>
+                    <p>Best environment for best quality treatment and service</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -31,8 +31,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>Pediatric dental service</h3>
+                    <p>Special care for baby and children</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -43,8 +43,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3>Healthy teeth for healthy life </h3>
+                    <p>Healthy teeth for perfect smile</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
